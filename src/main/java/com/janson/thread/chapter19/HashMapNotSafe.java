2289e4cc529df533b02c9e01046a7100aa6fe33a
janson <janson_j_shan@163.com>
2022-01-04 22:55:36 +0800

    Thread hashMap is not safe

---
+++ src/main/java/com/janson/thread/chapter19/HashMapNotSafe.java
@@ -0,0 +1,28 @@
+package com.janson.thread.chapter19;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+/**
+ * @Description:
+ * @Author: Janson
+ * @Date: 2022/1/4 22:39
+ **/
+public class HashMapNotSafe {
+    public static void main(String[] args) {
+        final Map<Integer, String> map = new HashMap<>();
+        // 65535
+        final Integer targetKey = 0b1111_1111_1111_1111;
+        final String targetValue = "v";
+        map.put(targetKey, targetValue);
+        new Thread(() -> {
+            IntStream.range(0, targetKey).forEach(key -> map.put(key, "someValue"));
+        }).start();
+        while (true) {
+            if (null == map.get(targetKey)) {
+                throw new RuntimeException("HashMap is not thread safe");
+            }
+        }
+    }
+}
