50c37ffad9224a8aaa61ac9810104dde0f3e0f37
janson <janson_j_shan@163.com>
2022-01-17 22:59:35 +0800

    为什么String被设计为是不可变的

---
+++ src/main/java/com/janson/thread/chapter45/StringTest.java
@@ -0,0 +1,17 @@
+package com.janson.thread.chapter45;
+
+/**
+ * @Description:
+ * @Author: Janson
+ * @Date: 2022/1/17 22:55
+ **/
+public class StringTest {
+
+    public static void main(String[] args) {
+        String s1 = "lagou";
+        String s2 = "lagou";
+        s1 = "LAGOU";
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
