8b3d40c37fd3f2d9b7804671428e01ed4c95e7b5
janson <janson_j_shan@163.com>
2022-01-19 11:57:11 +0800

    add two numbers

---
+++ src/main/java/com/janson/leetcode/AddTwoNumbers.java
@@ -0,0 +1,100 @@
+package com.janson.leetcode;
+
+/**
+ * @Description: 两数相加
+ * <p>
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * <p>
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * <p>
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * @Author: Janson
+ * @Date: 2022/1/19 10:36
+ **/
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        ListNode listNode = addTwoNumbers.addTwoNumbers(l1, l2);
+
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+
+
+    /**
+     * 由于输入的两个链表都是逆序存储数字的位数的，因此两个链表中同一位置的数字可以直接相加。
+     * <p>
+     * 我们同时遍历两个链表，逐位计算它们的和，并与当前位置的进位值相加。具体而言，如果当前两个链表处相应位置的数字为 n1,n2n1,n2，进位值为 \textit{carry}carry，则它们的和为 n1+n2+\textit{carry}n1+n2+carry；其中，答案链表处相应位置的数字为 (n1+n2+\textit{carry}) \bmod 10(n1+n2+carry)mod10，而新的进位值为 \lfloor\frac{n1+n2+\textit{carry}}{10}\rfloor⌊
+     * 10
+     * n1+n2+carry
+     * ​
+     * ⌋。
+     * <p>
+     * 如果两个链表的长度不同，则可以认为长度短的链表的后面有若干个 00 。
+     * <p>
+     * 此外，如果链表遍历结束后，有 \textit{carry} > 0carry>0，还需要在答案链表的后面附加一个节点，节点的值为 \textit{carry}carry。
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
+
+    static class ListNode {
+        private int val;
+        private ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+}
