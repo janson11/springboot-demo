3982c8d1401d4a1c780d625b84f78bb4bebdd2ab
shanjian <shanjian@codemao.com>
2022-04-12 17:55:04 +0800

    transaction message

---
+++ src/main/java/com/janson/rocketmq/producer/TransactionProducer.java
@@ -0,0 +1,58 @@
+package com.janson.rocketmq.producer;
+
+import com.janson.rocketmq.listener.TransactionListenerImpl;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.TransactionListener;
+import org.apache.rocketmq.client.producer.TransactionMQProducer;
+import org.apache.rocketmq.client.producer.TransactionSendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description:
+ * @Author: shanjian
+ * @Date: 2022/4/12 5:35 下午
+ */
+public class TransactionProducer {
+    public static void main(String[] args) throws InterruptedException {
+
+        TransactionListener transactionListener = new TransactionListenerImpl();
+        TransactionMQProducer producer = new TransactionMQProducer("TRANSACTION_PRODUCER_GROUP");
+        producer.setNamesrvAddr("127.0.0.1:9876");
+        ExecutorService executorService = new ThreadPoolExecutor(2, 5, 100, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(2000), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setName("client-transaction-check-thread");
+                return thread;
+            }
+        });
+
+        producer.setExecutorService(executorService);
+        producer.setTransactionListener(transactionListener);
+        String[] tags = new String[]{"TagA", "TagB", "TagC"};
+        for (int i = 0; i < 10; i++) {
+            try {
+                Message msg = new Message("TransactionTopic", tags[i % tags.length], "KEY" + i, ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+                TransactionSendResult sendResult = producer.sendMessageInTransaction(msg, null);
+                System.out.printf("%s%n", sendResult);
+                Thread.sleep(10);
+            } catch (UnsupportedEncodingException | InterruptedException | MQClientException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        for (int i1 = 0; i1 < 100000; i1++) {
+            Thread.sleep(1000);
+        }
+        producer.shutdown();
+    }
+}
