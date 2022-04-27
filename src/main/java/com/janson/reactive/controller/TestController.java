5326f6fbe7a101f7eaddf36bdae2abea98bc4cea
shanjian <shanjian@codemao.com>
2022-04-27 10:40:21 +0800

    Mono Flux

--- src/main/java/com/janson/reactive/controller/TestController.java
+++ src/main/java/com/janson/reactive/controller/TestController.java
@@ -3,8 +3,10 @@ package com.janson.reactive.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+import java.util.Optional;
 
 /**
  * @Description:
@@ -16,30 +18,46 @@ import java.time.Duration;
 public class TestController {
 
     public static void main(String[] args) {
-        Flux.just("Hello","World").subscribe(System.out::println);
+        Flux.just("Hello", "World").subscribe(System.out::println);
 
-        Flux.fromArray(new Integer[]{1,2,3}).subscribe(System.out::println);
+        Flux.fromArray(new Integer[]{1, 2, 3}).subscribe(System.out::println);
 
-        Flux.range(2020,5).subscribe(System.out::println);
+        Flux.range(2020, 5).subscribe(System.out::println);
 
-        Flux.interval(Duration.ofSeconds(2),Duration.ofMillis(200)).subscribe(System.out::println);
+        Flux.interval(Duration.ofSeconds(2), Duration.ofMillis(200)).subscribe(System.out::println);
 
         Flux.empty().subscribe(System.out::println);
 
 
-        Flux.generate( synchronousSink -> {
+        Flux.generate(synchronousSink -> {
             synchronousSink.next("janson");
             synchronousSink.complete();
         }).subscribe(System.out::println);
 
-        Flux.generate(() ->1,(i,sink)->{
+        Flux.generate(() -> 1, (i, sink) -> {
             sink.next(i);
-            if (i==5){
+            if (i == 5) {
                 sink.complete();
             }
             return ++i;
         }).subscribe(System.out::println);
 
 
+        Mono.justOrEmpty(Optional.of("janson")).subscribe(System.out::println);
+
+        Mono.create(monoSink ->
+                monoSink.success("janson1")).subscribe(System.out::println);
+
+        Mono.just("janson2")
+                .concatWith(Mono.error(new IllegalStateException()))
+                .subscribe(System.out::println,System.err::println);
+
+        Mono.just("janson3")
+                .concatWith(Mono.error(new IllegalStateException()))
+                .onErrorReturn("default")
+                .subscribe(System.out::println);
+
+        Flux.just("xiaoyunzi1","xiaoyunzi2","xiaoyunzi3")
+                .subscribe(data ->System.out.println("onNext:"+data),err ->{},()->System.out.println("onComplete"));
     }
 }
