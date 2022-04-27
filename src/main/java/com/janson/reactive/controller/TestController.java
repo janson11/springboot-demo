e1f13c9a7990efaee1289b151bcfd5e2631ea34e
shanjian <shanjian@codemao.com>
2022-04-27 11:31:33 +0800

    flux /mono operate

--- src/main/java/com/janson/reactive/controller/TestController.java
+++ src/main/java/com/janson/reactive/controller/TestController.java
@@ -50,14 +50,61 @@ public class TestController {
 
         Mono.just("janson2")
                 .concatWith(Mono.error(new IllegalStateException()))
-                .subscribe(System.out::println,System.err::println);
+                .subscribe(System.out::println, System.err::println);
 
         Mono.just("janson3")
                 .concatWith(Mono.error(new IllegalStateException()))
                 .onErrorReturn("default")
                 .subscribe(System.out::println);
 
-        Flux.just("xiaoyunzi1","xiaoyunzi2","xiaoyunzi3")
-                .subscribe(data ->System.out.println("onNext:"+data),err ->{},()->System.out.println("onComplete"));
+        Flux.just("xiaoyunzi1", "xiaoyunzi2", "xiaoyunzi3")
+                .subscribe(data -> System.out.println("onNext:" + data), err -> {
+                }, () -> System.out.println("onComplete"));
+
+        Flux.range(1, 25).buffer(10).subscribe(System.out::println);
+
+        Flux.range(1, 5).window(2).toIterable().forEach(w -> {
+            w.subscribe(System.out::println);
+            System.out.println("-------------");
+        });
+
+        Flux.just(1, 2).map(i -> "number-" + i).subscribe(System.out::println);
+
+        Flux.just(1, 5).flatMap(x -> Mono.just(x * x)).subscribe(System.out::println);
+
+        Flux.range(1, 10).filter(i -> i % 2 == 0).subscribe(System.out::println);
+
+        System.out.println("++++++++++++");
+
+        Flux.range(1, 100).take(10).subscribe(System.out::println);
+        Flux.range(1, 100).takeLast(10).subscribe(System.out::println);
+
+        Flux.just(1, 2, 3)
+                .then()
+                .subscribe(System.out::println);
+
+        Flux.just(1, 2, 3)
+                .thenMany(Flux.just(4, 5))
+                .subscribe(System.out::println);
+
+
+//        Flux.merge(Flux.interval(Duration.ofSeconds(0), Duration.ofSeconds(100)).take(2), Flux.interval(Duration.ofSeconds(50), Duration.ofSeconds(100)).take(2)).toStream()
+//                .forEach(System.out::println);
+
+        System.out.println("AAAAAAAA");
+
+//        Flux.mergeSequential(Flux.interval(Duration.ofSeconds(0), Duration.ofSeconds(100)).take(2), Flux.interval(Duration.ofSeconds(50), Duration.ofSeconds(100)).take(2)).toStream()
+//                .forEach(System.out::println);
+
+        Flux flux1 =Flux.just(1,2);
+        Flux flux2 =Flux.just(3,4);
+        Flux.zip(flux1,flux2).subscribe(System.out::println);
+
+
+        Flux.just(1,2).zipWith(Flux.just(3,4)).subscribe(System.out::println);
+
+        Flux.just(1,2).zipWith(Flux.just(3,4),(s1,s2)->String.format("%s+%s=%s",s1,s2,s1+s2)).subscribe(System.out::println);
+
+
     }
 }
