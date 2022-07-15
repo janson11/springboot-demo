b21aa13700d0b9049bc3916ca926693da35e1e36
shanjian <shanjian@codemao.com>
2022-07-15 10:03:37 +0800

    年轻代垃圾回收gc

---
+++ src/main/java/com/janson/jvm/demo/Demo4.java
@@ -0,0 +1,31 @@
+package com.janson.jvm.demo;
+
+/**
+ * @Description: JVM JDK 1.8 demo测试
+ * -XX:NewSize=10485760 -XX:MaxNewSize=10485760 -XX:InitialHeapSize=20971520 -XX:MaxHeapSize=20971520 -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15 -XX:PretenureSizeThreshold=10485760 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:gc3.log
+ * -XX:NewSize 初始新生代大小 5242880B=5MB
+ * -XX:MaxNewSize 最大新生代大小 5242880B=5MB
+ * -XX:InitialHeapSize 初始堆大小 10485760 = 10MB
+ * -XX:MaxHeapSize   最大堆大小 10485760 = 10MB
+ * -XX:SurvivorRatio=8 新生代Eden区与Survivor2区的占比，默认是8：1：1【Eden:Survior1:Survior2】
+ * -XX:PretenureSizeThreshold  大对象阈值 10485760=10MB
+ * <p>
+ * -XX:+PrintGCDetails：打印详细的gc日志
+ * -XX:+PrintGCTimeStamps：这个参数可以打印出来每次GC发生的时间
+ * -Xloggc:gc.log：这个参数可以设置将gc日志写入一个磁盘文件
+ * @Author: shanjian
+ * @Date: 2022/7/12 10:20 上午
+ */
+public class Demo4 {
+    public static void main(String[] args) {
+        byte[] array1 = new byte[2 * 1024 * 1024];
+        array1 = new byte[2 * 1024 * 1024];
+        array1 = new byte[2 * 1024 * 1024];
+
+
+        byte[] array2 = new byte[128 * 1024];
+        array2=null;
+
+        byte[] array3 = new byte[2 * 1024 * 1024];
+    }
+}
