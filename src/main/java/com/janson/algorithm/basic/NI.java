a59b582403f40daa1bd3f716d905dc11c4c9d942
shanjian <shanjian@codemao.com>
2022-08-15 10:28:09 +0800

    归并排序

---
+++ src/main/java/com/janson/algorithm/basic/NI.java
@@ -0,0 +1,13 @@
+package com.janson.algorithm.basic;
+
+/**
+ * @Description: 逆序对数问题
+ * 在一个数组中，左边的数如果比右边的数大，则折两个数构成一个逆序对，请打印所有的逆序对。
+ *
+ * https://www.nowcoder.com/questionTerminal/96bd6684e04a44eb80e6a68efc0ec6c5
+ *
+ * @Author: shanjian
+ * @Date: 2022/8/12 10:36 上午
+ */
+public class NI {
+}
