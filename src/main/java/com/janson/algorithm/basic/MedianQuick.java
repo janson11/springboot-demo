fc3543631c31c48dc23050b4ab1d6cf7d0114ef3
shanjian <shanjian@codemao.com>
2022-08-24 10:20:38 +0800

    大根堆和小根堆解决中位数问题

---
+++ src/main/java/com/janson/algorithm/basic/MedianQuick.java
@@ -0,0 +1,70 @@
+package com.janson.algorithm.basic;
+
+import org.apache.commons.lang3.builder.CompareToBuilder;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @Description: 大根堆与小根堆
+ * @Author: shanjian
+ * @Date: 2022/8/24 9:54 上午
+ */
+public class MedianQuick {
+
+    public static class MedianHolder {
+        private PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new MaxHeapComparator());
+        private PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(new MaxHeapComparator());
+
+        private void modifyTwoHeapSize() {
+            if (this.maxHeap.size() == this.minHeap.size() + 2) {
+                this.minHeap.add(this.maxHeap.poll());
+            }
+            if (this.minHeap.size() == this.maxHeap.size() + 2) {
+                this.maxHeap.add(this.minHeap.poll());
+            }
+        }
+
+        public void addNumber(int num) {
+            if (this.maxHeap.isEmpty()) {
+                this.maxHeap.add(num);
+                return;
+            }
+            if (this.maxHeap.peek() >= num) {
+                this.maxHeap.add(num);
+            } else {
+                this.minHeap.add(num);
+            }
+            modifyTwoHeapSize();
+        }
+
+        public Integer getMedian() {
+            int maxHeapSize = this.maxHeap.size();
+            int minHeapSize = this.minHeap.size();
+            if (maxHeapSize + minHeapSize == 0) {
+                return null;
+            }
+            Integer maxHeapHead = this.maxHeap.peek();
+            Integer minHeapHead = this.minHeap.peek();
+            if (((maxHeapSize + minHeapSize) & 1) == 0) {
+                return (maxHeapHead + minHeapHead) / 2;
+            }
+            return maxHeapSize > minHeapSize ? maxHeapHead : minHeapHead;
+        }
+    }
+
+
+    public static class MaxHeapComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            if (o2 > o1) {
+                return 1;
+            } else if (o2 < o1) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+}
