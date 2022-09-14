3ed20229174d8db7a8772f70892531d6302a8ea7
shanjian <shanjian@codemao.com>
2022-09-14 10:34:59 +0800

    maxGap

--- src/main/java/com/janson/algorithm/basic/BucketSort.java
+++
@@ -1,9 +0,0 @@
-package com.janson.algorithm.basic;
-
-/**
- * @Description: 给定一个数组，求如果排序之后，相邻两数的最大差值，要求时间复杂度O(N),且要求不能用非基于比较的排序【算法必考，借用桶排序但是没有使用桶，算法非常妙】
- * @Author: shanjian
- * @Date: 2022/9/7 10:15 上午
- */
-public class BucketSort {
-}
