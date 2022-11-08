99c58db052669b988374bbd23bf8305f85f11b90
shanjian <shanjian@codemao.com>
2022-11-08 11:40:07 +0800

    netty几种解码器示例

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ ChannelHandler通道处理器。在ChannelHandler通道处理器的入站、出�
 方法中，Netty都会传递一个Context上下文实例作为实际参数。通过Context实例
 的实参，在业务处理中，可以获取ChannelPipeline通道流水线的实例或者Channel
 通道的实例。
+
+
+### 2 、开箱即用的Netty内置Decoder
+ ####（1）固定长度数据包解码器——FixedLengthFrameDecoder
+适用场景：每个接收到的数据包的长度，都是固定的，例如100个字节。
+在这种场景下，只需要把这个解码器加到流水线中，它会把入站ByteBuf数据包拆分成一个个长度为100的数据包，然后发往下一个channelHandler入站处理器。补充说明一下：这里所指的一个数据包，在Netty中就是一个ByteBuf实例。注：数据帧（Frame），本书也通称为数据包。
+####（2）行分割数据包解码器——LineBasedFrameDecoder
+适用场景：每个ByteBuf数据包，使用换行符（或者回车换行符）作为数据包的边界分割符。
+如果每个接收到的数据包，都以换行符/回车换行符作为分隔。在这种场景下，只需要把这个解码器加到流水线中，Netty会使用换行分隔符，把ByteBuf数据包分割成一个一个完整的应用层ByteBuf数据包，再发送到下一站。
+####（3）自定义分隔符数据包解码器——DelimiterBasedFrameDecoder
+DelimiterBasedFrameDecoder是LineBasedFrameDecoder按照行分割的通用版本。不同之处在于，这个解码器更加灵活，可以自定义分隔符，而不是局限于换行符。如果使用这个解码器，那么接收到的数据包，末尾必须带上对应的分隔符。
+####（4）自定义长度数据包解码器——LengthFieldBasedFrameDecoder
+这是一种基于灵活长度的解码器。在ByteBuf数据包中，加了一个长度字段，保存了原始数据包的长度。解码的时候，会按照这个长度进行原始数据包的提取。
+这种解码器在所有开箱即用解码器中是最为复杂的一种，后面会重点介绍。
---
+++ netty-demos/src/main/java/com/janson/netty/demos/decoder/NettyOpenBoxDecoder.java
@@ -0,0 +1,193 @@
+package com.janson.netty.demos.decoder;
+
+import com.janson.netty.common.util.RandomUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import org.apache.commons.lang.math.RandomUtils;
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @Description:
+ * @Author: shanjian
+ * @Date: 2022/11/7 10:38 上午
+ */
+public class NettyOpenBoxDecoder {
+
+    public static final String SPLITER = "\r\n";
+    public static final String SPLITER_2 = "\t";
+
+    public static final String CONTENT = "疯狂创客圈：高性能学习社群";
+
+    /**
+     * 行分割数据包解码器
+     *
+     * @see io.netty.handler.codec.LineBasedFrameDecoder
+     */
+    @Test
+    public void testLineBasedDecoder() {
+        ChannelInitializer<EmbeddedChannel> channelInitializer = new ChannelInitializer<EmbeddedChannel>() {
+
+            @Override
+            protected void initChannel(EmbeddedChannel ch) throws Exception {
+                ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                ch.pipeline().addLast(new StringDecoder());
+                ch.pipeline().addLast(new StringProcessHandler());
+            }
+        };
+
+        EmbeddedChannel channel = new EmbeddedChannel(channelInitializer);
+        for (int i = 0; i < 100; i++) {
+            int random = RandomUtil.randInMod(3);
+            ByteBuf buf = Unpooled.buffer();
+            for (int j = 0; j < random; j++) {
+                buf.writeBytes(CONTENT.getBytes(StandardCharsets.UTF_8));
+            }
+            buf.writeBytes(SPLITER.getBytes(StandardCharsets.UTF_8));
+            channel.writeInbound(buf);
+        }
+
+        try {
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 自定义分隔符数据包解码器
+     *
+     * @see io.netty.handler.codec.DelimiterBasedFrameDecoder
+     */
+    @Test
+    public void testDelimiterBasedFrameDecoder() {
+        ByteBuf delimiter = Unpooled.copiedBuffer(SPLITER_2.getBytes(StandardCharsets.UTF_8));
+        ChannelInitializer<EmbeddedChannel> channelInitializer = new ChannelInitializer<EmbeddedChannel>() {
+
+            @Override
+            protected void initChannel(EmbeddedChannel ch) throws Exception {
+                ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, true, delimiter));
+                ch.pipeline().addLast(new StringDecoder());
+                ch.pipeline().addLast(new StringProcessHandler());
+            }
+        };
+
+        EmbeddedChannel channel = new EmbeddedChannel(channelInitializer);
+        for (int i = 0; i < 3; i++) {
+            int random = RandomUtil.randInMod(3);
+            ByteBuf buf = Unpooled.buffer();
+            for (int j = 0; j < random; j++) {
+                buf.writeBytes(CONTENT.getBytes(StandardCharsets.UTF_8));
+            }
+            buf.writeBytes(SPLITER_2.getBytes(StandardCharsets.UTF_8));
+            channel.writeInbound(buf);
+        }
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 自定义长度数据包解码器
+     *
+     * @see io.netty.handler.codec.LengthFieldBasedFrameDecoder
+     */
+    @Test
+    public void testLengthFieldBasedFrameDecoder() throws UnsupportedEncodingException {
+        // 定义一个基于长度域解码器
+        final LengthFieldBasedFrameDecoder decoder = new LengthFieldBasedFrameDecoder(1024, 0, 4, 0, 4);
+        ChannelInitializer<EmbeddedChannel> channelInitializer = new ChannelInitializer<EmbeddedChannel>() {
+
+            @Override
+            protected void initChannel(EmbeddedChannel ch) throws Exception {
+                ch.pipeline().addLast(decoder);
+                ch.pipeline().addLast(new StringDecoder(Charset.forName("UTF-8")));
+                ch.pipeline().addLast(new StringProcessHandler());
+            }
+        };
+
+        EmbeddedChannel channel = new EmbeddedChannel(channelInitializer);
+        for (int i = 0; i < 100; i++) {
+            int random = RandomUtil.randInMod(3);
+            ByteBuf buf = Unpooled.buffer();
+            byte[] bytes = CONTENT.getBytes("UTF-8");
+            // 首先，写入头部，head 也就是说后面的数据长度
+            buf.writeInt(bytes.length * random);
+
+
+            // 然后写入content
+            for (int j = 0; j < random; j++) {
+                buf.writeBytes(bytes);
+            }
+            channel.writeInbound(buf);
+        }
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 自定义长度数据包解码器
+     *   int maxFrameLength, 发送的数据包的最大长度
+     *   int lengthFieldOffset, 长度字段偏移量
+     *   int lengthFieldLength,  长度字段所占的字节数
+     *   int lengthAdjustment,  长度的矫正值
+     *   int initialBytesToStrip 丢弃的起始字节数
+     *
+     *
+     *
+     * @see io.netty.handler.codec.LengthFieldBasedFrameDecoder
+     */
+    @Test
+    public void testLengthFieldBasedFrameDecoder1() throws UnsupportedEncodingException {
+        // 定义一个基于长度域解码器
+        final LengthFieldBasedFrameDecoder decoder = new LengthFieldBasedFrameDecoder(1024, 0, 4, 0, 4);
+        ChannelInitializer<EmbeddedChannel> channelInitializer = new ChannelInitializer<EmbeddedChannel>() {
+
+            @Override
+            protected void initChannel(EmbeddedChannel ch) throws Exception {
+                ch.pipeline().addLast(decoder);
+                ch.pipeline().addLast(new StringDecoder(Charset.forName("UTF-8")));
+                ch.pipeline().addLast(new StringProcessHandler());
+            }
+        };
+
+        EmbeddedChannel channel = new EmbeddedChannel(channelInitializer);
+        for (int i = 1; i <= 100; i++) {
+            ByteBuf buf = Unpooled.buffer();
+            String s = i + "次发送->" + CONTENT;
+            byte[] bytes = s.getBytes("UTF-8");
+            // 首先，写入头部，head 也就是说后面的数据长度
+            buf.writeInt(bytes.length);
+            System.out.println("bytes length= " + bytes.length);
+            buf.writeBytes(bytes);
+            channel.writeInbound(buf);
+        }
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
