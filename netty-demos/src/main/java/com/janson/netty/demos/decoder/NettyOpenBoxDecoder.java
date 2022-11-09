e3eca3d804ecdbbfbb4a473e8347d7035da60358
shanjian <shanjian@codemao.com>
2022-11-09 10:09:24 +0800

    自定义长度数据包解码器

--- netty-demos/src/main/java/com/janson/netty/demos/decoder/NettyOpenBoxDecoder.java
+++ netty-demos/src/main/java/com/janson/netty/demos/decoder/NettyOpenBoxDecoder.java
@@ -9,7 +9,6 @@ import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
-import org.apache.commons.lang.math.RandomUtils;
 import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
@@ -23,6 +22,9 @@ import java.nio.charset.StandardCharsets;
  */
 public class NettyOpenBoxDecoder {
 
+
+    public static final int MAGICCODE = 9999;
+    public static final int VERSION = 100;
     public static final String SPLITER = "\r\n";
     public static final String SPLITER_2 = "\t";
 
@@ -188,6 +190,101 @@ public class NettyOpenBoxDecoder {
             e.printStackTrace();
         }
     }
+
+
+    /**
+     * 自定义长度数据包解码器
+     *   int maxFrameLength, 发送的数据包的最大长度
+     *   int lengthFieldOffset, 长度字段偏移量
+     *   int lengthFieldLength,  长度字段所占的字节数
+     *   int lengthAdjustment,  长度的矫正值
+     *   int initialBytesToStrip 丢弃的起始字节数
+     *
+     *
+     *
+     * @see io.netty.handler.codec.LengthFieldBasedFrameDecoder
+     */
+    @Test
+    public void testLengthFieldBasedFrameDecoder2() throws UnsupportedEncodingException {
+        // 定义一个基于长度域解码器
+        final LengthFieldBasedFrameDecoder spliter = new LengthFieldBasedFrameDecoder(1024, 0, 4, 2, 6);
+        ChannelInitializer<EmbeddedChannel> channelInitializer = new ChannelInitializer<EmbeddedChannel>() {
+
+            @Override
+            protected void initChannel(EmbeddedChannel ch) throws Exception {
+                ch.pipeline().addLast(spliter);
+                ch.pipeline().addLast(new StringDecoder(Charset.forName("UTF-8")));
+                ch.pipeline().addLast(new StringProcessHandler());
+            }
+        };
+
+        EmbeddedChannel channel = new EmbeddedChannel(channelInitializer);
+        for (int i = 1; i <= 100; i++) {
+            ByteBuf buf = Unpooled.buffer();
+            String s = i + "次发送->" + CONTENT;
+            byte[] bytes = s.getBytes("UTF-8");
+            // 首先，写入头部，head 也就是说后面的数据长度
+            buf.writeChar(VERSION);
+            buf.writeInt(bytes.length);
+            System.out.println("bytes length= " + bytes.length);
+            buf.writeBytes(bytes);
+            channel.writeInbound(buf);
+        }
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 自定义长度数据包解码器
+     *   int maxFrameLength, 发送的数据包的最大长度
+     *   int lengthFieldOffset, 长度字段偏移量
+     *   int lengthFieldLength,  长度字段所占的字节数
+     *   int lengthAdjustment,  长度的矫正值
+     *   int initialBytesToStrip 丢弃的起始字节数
+     *
+     *
+     *
+     * @see io.netty.handler.codec.LengthFieldBasedFrameDecoder
+     */
+    @Test
+    public void testLengthFieldBasedFrameDecoder3() throws UnsupportedEncodingException {
+        // 定义一个基于长度域解码器
+        final LengthFieldBasedFrameDecoder spliter = new LengthFieldBasedFrameDecoder(1024, 2, 4, 4, 10);
+        ChannelInitializer<EmbeddedChannel> channelInitializer = new ChannelInitializer<EmbeddedChannel>() {
+
+            @Override
+            protected void initChannel(EmbeddedChannel ch) throws Exception {
+                ch.pipeline().addLast(spliter);
+                ch.pipeline().addLast(new StringDecoder(Charset.forName("UTF-8")));
+                ch.pipeline().addLast(new StringProcessHandler());
+            }
+        };
+
+        EmbeddedChannel channel = new EmbeddedChannel(channelInitializer);
+        for (int i = 1; i <= 100; i++) {
+            ByteBuf buf = Unpooled.buffer();
+            String s = i + "次发送->" + CONTENT;
+            byte[] bytes = s.getBytes("UTF-8");
+            // 首先，写入头部，head 也就是说后面的数据长度
+            buf.writeChar(VERSION);
+            buf.writeInt(bytes.length);
+            buf.writeInt(MAGICCODE);
+            System.out.println("bytes length= " + bytes.length);
+            buf.writeBytes(bytes);
+            channel.writeInbound(buf);
+        }
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
