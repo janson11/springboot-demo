42076ff911f2cad08051970ff0f8d8015cf59a58
janson11 <SJ520zxy=1314>
2022-12-08 09:18:54 +0800

    jvm 字节码demo

---
+++ src/main/java/com/janson/jvm/HelloByteCode.java
@@ -0,0 +1,13 @@
+package com.janson.jvm;
+
+/**
+ * @Description:
+ * @Author: Janson
+ * @Date: 2022/12/8 9:06
+ **/
+public class HelloByteCode {
+    public static void main(String[] args) {
+        // jclasslib 字节码插件查看字节码
+        HelloByteCode obj = new HelloByteCode();
+    }
+}
