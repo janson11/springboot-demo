ce616c92eb0b9e170f977131528dab1692968b24
shanjian <shanjian@codemao.com>
2023-05-15 15:47:34 +0800

    add junit test 2

--- src/test/java/com/janson/springboot/demo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/janson/springboot/demo/SpringbootDemoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.janson.springboot.demo;
 
 import org.junit.jupiter.api.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.concurrent.CountDownLatch;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringbootDemoApplicationTests {
 
+    Logger logger = LoggerFactory.getLogger(SpringbootDemoApplicationTests.class);
+
+    //logback  14231
+    //log4j  14469
+    //log4j2  19642
     @Test
-    void contextLoads() {
+    public void testLog() {
+        int threadNum = 100;
+        final int LOOP_NUM = 100000;
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < threadNum; i++) {
+            for (int j = 0; j < LOOP_NUM; j++) {
+                logger.info("Info Message j:{}", j);
+            }
+        }
+        System.out.println(System.currentTimeMillis() - start);
     }
 
 }
