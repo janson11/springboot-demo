d5fc740838f91225146728d16dec9783fa583029
janson11 <janson_j_shan@163.com>
2023-07-08 09:24:46 +0800

    init jmeter

---
+++ src/main/java/com/janson/springboot/demo/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.janson.springboot.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: user controller
+ * @Author: Janson
+ * @Date: 2023/7/8 9:19
+ **/
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+
+}
