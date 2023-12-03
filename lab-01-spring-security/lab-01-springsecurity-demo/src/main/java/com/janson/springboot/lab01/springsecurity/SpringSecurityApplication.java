98e676c7d78407e55960a06f1d07fddee73a4386
janson11 <janson_j_shan@163.com>
2023-12-03 22:36:22 +0800

    lab-01-spring-security demo

---
+++ lab-01-spring-security/lab-01-springsecurity-demo/src/main/java/com/janson/springboot/lab01/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,17 @@
+package com.janson.springboot.lab01.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: Janson
+ * @Date: 2023/12/3 22:15
+ **/
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+}
---
+++ lab-01-spring-security/lab-01-springsecurity-demo/src/main/java/com/janson/springboot/lab01/springsecurity/controller/AdminController.java
@@ -0,0 +1,20 @@
+package com.janson.springboot.lab01.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: Janson
+ * @Date: 2023/12/3 22:19
+ **/
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "示例返回";
+    }
+}
