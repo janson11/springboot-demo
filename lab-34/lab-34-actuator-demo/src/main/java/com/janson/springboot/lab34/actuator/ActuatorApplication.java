adab9f555ab79872f7507ec4138d24a04d570297
shanjian <shanjian@codemao.com>
2023-12-05 20:25:24 +0800

    feat:lab-34-actuator-demo

---
+++ lab-34/lab-34-actuator-demo/src/main/java/com/janson/springboot/lab34/actuator/ActuatorApplication.java
@@ -0,0 +1,16 @@
+package com.janson.springboot.lab34.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: shanjian
+ * @Date: 2023/12/5 4:51 PM
+ */
+@SpringBootApplication
+public class ActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+}
