b35830c3ff4756bc15269e35baf0080a5ad4785f
shanjian <shanjian@codemao.com>
2023-12-08 11:38:11 +0800

    feat:lab-34-actuator-demo-httptrace

---
+++ lab-34/lab-34-actuator-demo-httptrace/src/main/java/com/janson/springboot/lab34/actuator/httptrace/config/ActuateConfig.java
@@ -0,0 +1,20 @@
+package com.janson.springboot.lab34.actuator.httptrace.config;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @Author: shanjian
+ * @Date: 2023/12/8 11:29 AM
+ */
+@Configuration
+public class ActuateConfig {
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
+}
