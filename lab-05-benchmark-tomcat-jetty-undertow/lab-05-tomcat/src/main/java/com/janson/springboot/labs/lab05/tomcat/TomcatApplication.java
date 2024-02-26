a9b7cf4ef73ec8e8f412f9c33ace5199d60c09aa
shanjian <shanjian@codemao.com>
2024-02-26 19:06:57 +0800

    feat:lab-05-tomcat

---
+++ lab-05-benchmark-tomcat-jetty-undertow/lab-05-tomcat/src/main/java/com/janson/springboot/labs/lab05/tomcat/TomcatApplication.java
@@ -0,0 +1,17 @@
+package com.janson.springboot.labs.lab05.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: shanjian
+ * @Date: 2024/2/26 17:11
+ */
+@SpringBootApplication
+public class TomcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomcatApplication.class, args);
+    }
+}
