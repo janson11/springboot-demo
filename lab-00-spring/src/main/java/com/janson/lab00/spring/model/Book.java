d2d449011609859bbb7cbe2865deff2e436a7761
janson11 <janson_j_shan@163.com>
2024-12-23 09:34:59 +0800

    xml注入其他特殊属性

--- lab-00-spring/src/main/java/com/janson/lab00/spring/model/Book.java
+++ lab-00-spring/src/main/java/com/janson/lab00/spring/model/Book.java
@@ -12,6 +12,8 @@ public class Book {
 
     private String bauthor;
 
+    private String address;
+
     // set方法注入
     public void setBname(String bname) {
         this.bname = bname;
@@ -21,9 +23,12 @@ public class Book {
         this.bauthor = bauthor;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public void testDemo() {
-        System.out.println(bname + ":" + bauthor);
+        System.out.println(bname + ":" + bauthor + ":" + address);
     }
 
     public static void main(String[] args) {
